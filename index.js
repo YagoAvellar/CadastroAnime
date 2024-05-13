e6896ebc4a7611020abc45a32ea0386e5ba64c9f
Avellar <99830352+YagoAvellar@users.noreply.github.com>
2024-05-13 12:17:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
         <body>
             <h1>Menu</h1>
             <ul>
-                <li><a href="ControleFinanceiro.html">Cadastrar novas despesas</a></li>
+                <li><a href="/cadastro">Cadastrar novas despesas</a></li>
                 <li><a href="/ControleFinanceiro">Listar Desepesas</a></li>
             </ul>
         </body>
@@ -88,7 +88,75 @@ function controller(req, res) {
     `;
     res.end(ConteudoResultado);
 }
+function cadastrarDespesa(req, res) {
+    if (req.query.produto && req.query.preco && req.query.categoria && req.query.descricao && req.query.data) {
+        const despesa = {
+            produto: req.query.produto,
+            preco: req.query.preco,
+            categoria: req.query.categoria,
+            descricao: req.query.descricao,
+            data: req.query.data
+        }
+        list.push(despesa);
 
+        res.writeHead(302, {
+            'Location': '/ControleFinanceiro' 
+        });
+        return res.end();
+    }
+
+    res.end(`
+    <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Controle Financeiro</title>
+</head>
+<body>
+    <form class="row p-3 justify-content-center" action="/ControleFinanceiro" method="get">
+      <h1 class="row justify-content-center m-3">Controle Financeiro</h1>
+        <div class="col-auto">
+          <label for="staticEmail2" class="visually-hidden">Nome do Produto</label>
+          <input type="text" name="produto" class="form-control" id="inputPreco" placeholder="Nome do Produto">
+        </div>
+        <div class="col-auto">
+          <label for="inputPassword2" class="visually-hidden">Valor</label>
+          <input type="text" name="preco" class="form-control" id="InputValor" placeholder="Valor">
+        </div>
+        <div class="col-auto">
+          <label for="inputDescricao" class="visually-hidden">Descrição</label>
+          <input type="text" name="descricao" class="form-control" id="inputDescricao" placeholder="Descrição do Produto">
+      </div>
+      <div class="col-auto">
+          <label for="inputCategoria" class="visually-hidden">Categoria</label>
+          <select class="form-select" name="categoria" id="inputCategoria">
+              <option selected>Selecione uma categoria</option>
+              <option value="Alimentação">Alimentação</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Moradia">Moradia</option>
+          </select>
+      </div>
+      <div class="col-auto">
+          <label for="inputData" class="visually-hidden">Data</label>
+          <input type="date" name="data" class="form-control" id="inputData">
+      </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-success mb-3">Salvar</button>
+        </div>
+        <div class="col-auto">
+          <button  class="btn btn-success mb-3" href="/ControleFinanceiro" role="button">Listar Despesas</button>
+        </div>
+
+      </form>
+</body>
+</html>
+    `);
+}
+
+app.get('/cadastro', cadastrarDespesa);
 app.get('/ControleFinanceiro', controller);
 
 app.listen(port, host, () => {
